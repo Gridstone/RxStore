490739f7222d8c5ddd3c15207ebd70b34bd75943
Christopher Horner <horner.cd@gmail.com>
2014-09-25 14:23:27 +1000

    Add base project layout

---
+++ grex-example/src/main/java/com/example/grex/ExampleActivity.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) GRIDSTONE 2014
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.grex;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.lang.Override;
+
+public class ExampleActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+    }
+}
