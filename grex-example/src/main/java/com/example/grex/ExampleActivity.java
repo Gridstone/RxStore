ce5fff94d707e9e68f5528792443b87538236286
Christopher Horner <horner.cd@gmail.com>
2014-12-03 09:37:06 +1100

    Example now uses GsonConverter, as the name is G-Rex after all

--- grex-example/src/main/java/com/example/grex/ExampleActivity.java
+++ grex-example/src/main/java/com/example/grex/ExampleActivity.java
@@ -25,7 +25,6 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.GsonConverter;
-import com.example.JacksonConverter;
 import com.example.grex.helpers.ArmLengthAdapter;
 import com.example.grex.helpers.DinoView;
 import com.example.grex.helpers.RandomDinoGenerator;
@@ -64,7 +63,7 @@ public class ExampleActivity extends Activity {
 
         setContentView(R.layout.activity_example);
         ButterKnife.inject(this);
-        dinoPersister = new GRexPersister(this, PERSISTENCE_DIR, new JacksonConverter());
+        dinoPersister = new GRexPersister(this, PERSISTENCE_DIR, new GsonConverter());
 
         loadDino();
         loadDinoList();
