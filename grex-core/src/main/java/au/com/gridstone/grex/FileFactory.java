0afd1a60a1b11e623203842ea5e56fb7876ad097
Joseph Cooper <cooper.joseph@gmail.com>
2014-12-09 00:53:41 +0100

    Created new grex-core subproject

--- grex-core/src/main/java/au/com/gridstone/grex/FileFactory.java
+++ grex-core/src/main/java/au/com/gridstone/grex/FileFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) GRIDSTONE 2014
+ * Copyright 2014 Omricat Software
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,20 @@
  * limitations under the License.
  */
 
-include ':grex'
-include ':grex-example'
-include ':converters:gson-converter'
-include ':converters:jackson-converter'
-include ':grex-core'
+package au.com.gridstone.grex;
+
+import java.io.File;
+
+/**
+ * Interface to provide a {@link File} given a {@code String} key.
+ *
+ * @author Joseph Cooper
+ */
+public interface FileFactory {
+
+    /**
+     * Return a {@link File} from a {@code String} key.
+     * Must not return null.
+     */
+    public File getFile(final String key);
+}
---
+++ grex-core/src/main/java/au/com/gridstone/grex/core/GRexCorePersister.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2014 Omricat Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.com.gridstone.grex.core;
+
+import java.io.File;
+
+import au.com.gridstone.grex.BaseGRexPersister;
+import au.com.gridstone.grex.FileIODelegate;
+import au.com.gridstone.grex.converter.Converter;
+
+
+public class GRexCorePersister extends BaseGRexPersister {
+
+    /**
+     * Create a new instance using a provided {@link Converter}.
+     *
+     * @param converter Converter used to serialize/deserialize objects, not
+     *                  null
+     * @param directory Directory to write/read files, not null. {@link
+     *                  File#isDirectory()} must return true on this parameter
+     */
+    public GRexCorePersister(final Converter converter,
+                             final File directory) {
+        super(converter, new FileIODelegate(directory) {
+        });
+    }
+
+}
